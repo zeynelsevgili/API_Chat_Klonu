79a8a36c5029eef6dda00e082ce092d99d5988a2
smart <zeynel.sevgili@gmail.com>
2018-09-05 05:33:50 +0300

    Initial Commit

---
+++ API_Chat_Klonu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  API_Chat_Klonu
+//
+//  Created by Demo on 5.09.2018.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
